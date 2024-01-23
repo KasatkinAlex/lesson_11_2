82b6eb67f7b94c17725bd07ac7d3f60354caf3b9
Alex Kasatkin <akasatkin1990@mail.ru>
2024-01-23 16:37:25 +0400

    add new func (first_capital) in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def all_caps(word:str):
+    """
+    Все заглавные
+    """
     return print(word.upper())
+
+def first_capital(word:str):
+    """
+    Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
+    """
+    return print(word.title())
